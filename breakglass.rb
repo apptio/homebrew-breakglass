c53b360dd7fb61b240d563293b66cec8aa6d5cc4
Lee Briggs <lbriggs@apptio.com>
2017-10-12 09:33:06 -0700

    Actually release 0.3.0

--- breakglass.rb
+++ breakglass.rb
@@ -7,11 +7,11 @@
 class Breakglass < Formula
   desc "A command line tool to provide login credentials from Hashicorp Vault"
   homepage "https://github.com/apptio/breakglass"
-  version "0.3.0-752aaef"
+  version "0.3.0-80eb8c7"
 
   if Hardware::CPU.is_64_bit?
-    url "https://github.com/apptio/breakglass/releases/download/v0.3.0-752aaef/breakglass_0.3.0-752aaef_darwin_amd64.zip"
-    sha256 "f2dee9153e7b53ebf5d68f1148528931e01fa818bd78e4f14af1dec983f79541"
+    url "https://github.com/apptio/breakglass/releases/download/v0.3.0-80eb8c7/breakglass_0.3.0-80eb8c7_darwin_amd64.zip"
+    sha256 "fe44a8ca7b9f0852e0077d1f4cc1c017eaf928841c04af399c2169d54a467fcb"
   end
 
   def install
